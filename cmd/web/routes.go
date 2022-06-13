b749f4af0f0039c58979e3b6dc0d013a2f979175
mateusjunges <mateus@junges.dev>
2022-06-12 22:20:44 -0300

    Added static images + created a file server

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,5 +19,9 @@ func Routes(app *config.AppConfig) http.Handler {
 	mux.Get("/home", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
